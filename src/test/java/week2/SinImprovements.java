a7101413f5ff12c620e4983e85296b71ce5ccf21
osamosonok <osamosonok@playtika.com>
2022-08-15 17:19:07 +0300

    week 2 - 1.8

--- src/test/java/week2/SinImprovements.java
+++ src/test/java/week2/SinImprovements.java
@@ -24,20 +24,28 @@ public class SinImprovements {
 
     void sinxImproved(int N, int terms, float[] x, float[] result) {
         int denomValues[] = new int[terms];
-        denomValues[0] = -6;
+        denomValues[0] = 1;
         int sign = -1;
 
-        for (int index = 1; index < terms; index++) {
-            denomValues[index] = sign / (2*index+2) * (2*index+3);
+        //cache denumerator value with sign (1, 3!, 5!,...)
+        for (int index = 0; index < terms - 1; index++) {
+            denomValues[index + 1] = sign / (2*index+2) * (2*index+3);
             sign = - sign;
         }
 
         for (int i = 0; i < N; i++) {
-            float value = x[i];
-            float numer = x[i] * x[i] * x[i];
+            float value = 0;
+
+            //create cache for all numerators of single x[i] (x[i], x[i]^3, x[i]^5, ....)
+            float numerValuesForSingleX[] = new float[terms];
+            numerValuesForSingleX[0] = x[i];
+            for (int index = 1; index < terms; index++) {
+                numerValuesForSingleX[index] = numerValuesForSingleX[index - 1] * x[i] * x[i];
+            }
+
+            //count result value
             for (int j = 0; j < terms; j++) {
-                value += numer / denomValues[j];
-                numer *= x[i] * x[i];
+                value += numerValuesForSingleX[j] / denomValues[j];
             }
             result[i] = value;
         }
