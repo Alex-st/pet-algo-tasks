ad2474e5fed19386f41a300f72bd18278e739959
osamosonok <osamosonok@playtika.com>
2022-08-15 14:59:44 +0300

    week 2 - 1.3

--- src/test/java/week2/IsDarkImprovements.java
+++ src/test/java/week2/IsDarkImprovements.java
@@ -3,8 +3,6 @@ package week2;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
-import java.util.Objects;
-
 @RunWith(JUnit4.class)
 public class IsDarkImprovements {
     int N = 4096;
@@ -22,35 +20,13 @@ public class IsDarkImprovements {
         return count < N * N / 2;
     }
 
-    public boolean isDarkCached() {
-        var count = 0;
-        var cacheCellIsLight = new Boolean[256];
-
-        for (int j = 0; j < N; ++j) {
-            for (int i = 0; i < N; ++i) {
-                byte color = image[i][j];
-                if (Objects.isNull(cacheCellIsLight[color])) {
-                    if (image[i][j] >= 128) {
-                        count += 1;
-                        cacheCellIsLight[color] = true;
-                    }
-                } else {
-                    if (cacheCellIsLight[color]) {
-                        count++;
-                    }
-                }
-
-            }
-        }
-        return count < N * N / 2;
-    }
-
     public boolean isDarkOptimized() {
         var lightCount = 0;
         var darkCount = 0;
         while (lightCount <= N * N / 2 && darkCount <= N * N / 2) {
-            for (int j = 0; j < N; ++j) {
-                for (int i = 0; i < N; ++i) {
+            // starting reading by i not j 
+            for (int i = 0; i < N; ++i) {
+                for (int j = 0; j < N; ++j) {
                     if (image[i][j] >= 128) {
                         lightCount += 1;
                     } else {
