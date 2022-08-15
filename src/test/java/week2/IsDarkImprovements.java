353d227f498074ec9b7b66637ee574f7fb74e055
osamosonok <osamosonok@playtika.com>
2022-08-15 15:11:33 +0300

    week 2 - 1.4

--- src/test/java/week2/IsDarkImprovements.java
+++ src/test/java/week2/IsDarkImprovements.java
@@ -3,10 +3,14 @@ package week2;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 @RunWith(JUnit4.class)
 public class IsDarkImprovements {
     int N = 4096;
-    byte image[][] = new byte[N][N];
+    Byte image[][] = new Byte[N][N];
 
     public boolean isDark() {
         var count = 0;
@@ -24,7 +28,7 @@ public class IsDarkImprovements {
         var lightCount = 0;
         var darkCount = 0;
         while (lightCount <= N * N / 2 && darkCount <= N * N / 2) {
-            // starting reading by i not j 
+            // starting reading by i not j
             for (int i = 0; i < N; ++i) {
                 for (int j = 0; j < N; ++j) {
                     if (image[i][j] >= 128) {
@@ -32,6 +36,28 @@ public class IsDarkImprovements {
                     } else {
                         darkCount +=1;
                     }
+                }
+            }
+        }
+
+        return lightCount < N * N / 2;
+    }
+
+    public boolean isDarkJavaOptimized() {
+        var lightCount = 0;
+        var darkCount = 0;
+
+        List<Byte> list = new ArrayList<>();
+        for (Byte[] array : image) {
+            list.addAll(Arrays.asList(array));
+        }
+
+        while (lightCount <= N * N / 2 && darkCount <= N * N / 2) {
+            for (int i = 0; i < N * N; ++i) {
+                if (list.get(i) >= 128) {
+                    lightCount += 1;
+                } else {
+                    darkCount +=1;
                 }
             }
         }
